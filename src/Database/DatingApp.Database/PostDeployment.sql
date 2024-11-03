890ca34b44598e0cb5b3b2a29fc6fbba29dee699
NaveeenSemwal <semwal.dotnet@gmail.com>
2024-11-03 20:00:10 +0530

    Added Database deployment scripts

---
+++ src/Database/DatingApp.Database/PostDeployment.sql
@@ -0,0 +1,6 @@
+﻿
+
+-- Please add your scripts to the 001\Data folder and add an entry to the 001\Release.sql to deploy it for the current version
+ 
+ 
+:r .\Scripts\001\Release.sql
---
+++ src/Database/DatingApp.Database/Scripts/001/Release.sql
@@ -0,0 +1,5 @@
+﻿PRINT 'Deploy version 001 data scripts';
+ 
+RAISERROR('2000 Initial Database script for master tables.sql', 1, 1)
+:r ".\Data\2000 Initial Database script for master tables.sql"
+GO
